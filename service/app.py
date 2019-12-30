73e52ff69d49b4adc358eef49440f127e848fcb4
mahosaurus <full@metal.pl>
2019-12-30 21:10:59 +0100

    dry

--- service/app.py
+++ service/app.py
@@ -37,6 +37,26 @@ except Exception as exc:
     windows = False
     print("Linux")
 
+def wrapper(request_form, bruttolohn=0):
+    if bruttolohn != 0:
+        request.form['f_bruttolohn'] = bruttolohn
+    return request_form.request_website(f_bruttolohn=request.form['f_bruttolohn'],
+                                        f_abrechnungszeitraum=request.form['f_abrechnungszeitraum'],
+                                        f_geld_werter_vorteil=request.form['f_geld_werter_vorteil'],
+                                        f_abrechnungsjahr=request.form['f_abrechnungsjahr'],
+                                        f_steuerfreibetrag=request.form['f_steuerfreibetrag'],
+                                        f_steuerklasse=request.form['f_steuerklasse'],
+                                        f_kirche=request.form['f_kirche'],
+                                        f_bundesland=request.form['f_bundesland'],
+                                        f_alter=request.form['f_alter'],
+                                        f_kinder=request.form['f_kinder'],
+                                        f_kinderfreibetrag=request.form['f_kinderfreibetrag'],
+                                        f_krankenversicherung=request.form['f_krankenversicherung'],
+                                        f_private_kv=request.form['f_private_kv'],
+                                        f_arbeitgeberzuschuss_pkv=request.form['f_arbeitgeberzuschuss_pkv'],
+                                        f_KVZ=request.form['f_KVZ'],
+                                        f_rentenversicherung=request.form['f_rentenversicherung'],
+                                        f_arbeitslosenversicherung=request.form['f_arbeitslosenversicherung'])
 
 if __name__ == '__main__':
     app = Flask(__name__)
@@ -46,27 +66,13 @@ if __name__ == '__main__':
     @app.route('/', methods=['GET', 'POST'])
     def provide():
         if request.method == "POST":
+
             # Single request
             if "[" not in request.form['f_bruttolohn']:
-                respons = request_form.request_website(f_bruttolohn=request.form['f_bruttolohn'],
-                                                       f_abrechnungszeitraum=request.form['f_abrechnungszeitraum'],
-                                                       f_geld_werter_vorteil=request.form['f_geld_werter_vorteil'],
-                                                       f_abrechnungsjahr=request.form['f_abrechnungsjahr'],
-                                                       f_steuerfreibetrag=request.form['f_steuerfreibetrag'],
-                                                       f_steuerklasse=request.form['f_steuerklasse'],
-                                                       f_kirche=request.form['f_kirche'],
-                                                       f_bundesland=request.form['f_bundesland'],
-                                                       f_alter=request.form['f_alter'],
-                                                       f_kinder=request.form['f_kinder'],
-                                                       f_kinderfreibetrag=request.form['f_kinderfreibetrag'],
-                                                       f_krankenversicherung=request.form['f_krankenversicherung'],
-                                                       f_private_kv=request.form['f_private_kv'],
-                                                       f_arbeitgeberzuschuss_pkv=request.form['f_arbeitgeberzuschuss_pkv'],
-                                                       f_KVZ=request.form['f_KVZ'],
-                                                       f_rentenversicherung=request.form['f_rentenversicherung'],
-                                                       f_arbeitslosenversicherung=request.form['f_arbeitslosenversicherung'])
+                respons = wrapper(request_form)
                 respons = respons.decode("ISO-8859-1")
                 parsed_respons = parse_res.parse_res(respons)
+                
             # Range request
             else:
                 range_to_check = re.sub(r'[^a-zA-Z\d\s\,\.]+', '', request.form['f_bruttolohn'])
@@ -76,24 +82,10 @@ if __name__ == '__main__':
                 for i in range(int(range_to_check[0].strip()),
                                int(range_to_check[1].strip()),
                                int(request.form['step'])):
+
                     app.logger.info("Getting" + str(i))
-                    respons = request_form.request_website(f_bruttolohn=i,
-                                                           f_abrechnungszeitraum=request.form['f_abrechnungszeitraum'],
-                                                           f_geld_werter_vorteil=request.form['f_geld_werter_vorteil'],
-                                                           f_abrechnungsjahr=request.form['f_abrechnungsjahr'],
-                                                           f_steuerfreibetrag=request.form['f_steuerfreibetrag'],
-                                                           f_steuerklasse=request.form['f_steuerklasse'],
-                                                           f_kirche=request.form['f_kirche'],
-                                                           f_bundesland=request.form['f_bundesland'],
-                                                           f_alter=request.form['f_alter'],
-                                                           f_kinder=request.form['f_kinder'],
-                                                           f_kinderfreibetrag=request.form['f_kinderfreibetrag'],
-                                                           f_krankenversicherung=request.form['f_krankenversicherung'],
-                                                           f_private_kv=request.form['f_private_kv'],
-                                                           f_arbeitgeberzuschuss_pkv=request.form['f_arbeitgeberzuschuss_pkv'],
-                                                           f_KVZ=request.form['f_KVZ'],
-                                                           f_rentenversicherung=request.form['f_rentenversicherung'],
-                                                           f_arbeitslosenversicherung=request.form['f_arbeitslosenversicherung'])
+
+                    respons = wrapper(request_form, bruttolohn=i)
                     respons = respons.decode("ISO-8859-1")
                     results.append(parse_res.parse_res(respons))
 
@@ -113,6 +105,7 @@ if __name__ == '__main__':
             # Get Request
             parsed_respons = {}
         return render_template("main.html", result=parsed_respons)
+
 
     app.logger.info("Starting app...")
     # DEBUG
